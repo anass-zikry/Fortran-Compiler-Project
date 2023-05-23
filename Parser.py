5c9f03ef14e48ca416eb5bbd0be4085b335647da
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2023-05-23 13:12:55 +0300

    fixed dfa string

--- Parser.py
+++ Parser.py
@@ -1219,13 +1219,14 @@ def Scan():
                     tok_string_string=tok_string_string+i
                 elif (re.match("[a-z]",i)):
                     tok_string_string=tok_string_string+"L"
-                elif (re.match("[a-z]",i)):
+                elif (re.match("[0-9]",i)):
                     tok_string_string=tok_string_string+"N"
                 elif(re.match("[\$%#@!]",i)):
-                    tok_string_string=tok_string_string+"Sy"
+                    tok_string_string=tok_string_string+"Y"
                 elif(re.match("\s",i)):
-                    tok_string_string=tok_string_string+"\S"
-                        
+                    tok_string_string=tok_string_string+"S"
+                  
+            print(tok_string_string)      
             string_dfa.show_diagram(input_str=tok_string_string,font_size=9, arrow_size=0.2,format_type='pdf',path="Diagrams/",filename=temp["Lex"],view=True)        
         elif temp['Lex'] in Operators:
             pass
